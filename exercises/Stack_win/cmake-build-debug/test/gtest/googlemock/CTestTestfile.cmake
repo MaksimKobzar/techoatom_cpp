0d0388f1baa28c9205780c7406d4cfbdad451c38
MaximKobzar <wtf__wow@mail.ru>
2017-03-04 00:43:38 +0300

    [~] improve hierarchy

--- exercises/Stack_win/cmake-build-debug/test/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maksim/git/techoatom_cpp/exercises/Stack_win/test/gtest/googlemock
-# Build directory: /home/maksim/git/techoatom_cpp/exercises/Stack_win/cmake-build-debug/test/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
