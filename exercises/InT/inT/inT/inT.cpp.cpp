03faf3d0a9164116e137d7a7111ef252db318c83
Alexmifi <mifi123456@mail.ru>
2017-03-06 22:30:17 +0300

    вот

---
+++ exercises/InT/inT/inT/inT.cpp.cpp
@@ -0,0 +1,17 @@
+#include "inT.h"
+
+int main() {
+	inT a,b,c,d;
+	a = 2;
+	b = 10;
+	c = 2;
+	c =d*( b + a)-b;
+	d = b / a;
+	a = a - 5;
+	printf("%d\n", d);
+	printf("%d\n", c);
+	printf("%d\n", a);
+	printf("%d\n", d);
+	system("pause");
+	return 0;
+}
