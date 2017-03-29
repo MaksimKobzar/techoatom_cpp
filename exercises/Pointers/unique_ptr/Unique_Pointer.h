4d3a1603fbb39e5f10db40009e8a59eb0e1f0911
Gerasimchuk Aleksey <gerasimchukaleksey@gmail.com>
2017-03-29 17:43:46 +0300

    added pointers

---
+++ exercises/Pointers/unique_ptr/Unique_Pointer.h
@@ -0,0 +1,104 @@
+﻿#include "stdafx.h"
+
+template <typename T>
+class unique_ptr
+{
+public:
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Element type
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	typedef T value_type;
+
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Constructor
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	unique_ptr();
+
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Constructor with pointer
+	//! @param pointer
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	unique_ptr(value_type* pointer);
+
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Disable copy constructor
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	unique_ptr(unique_ptr<value_type>& other) = delete;
+
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Destructor
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	~unique_ptr();
+
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Disable operator =
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	unique_ptr<value_type> operator=(unique_ptr<value_type>& other) = delete;
+
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Operator *, dereferences the unique_ptr
+	//! @return result of dereferencing the unique_ptr
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	value_type operator*() const;
+
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Returns the stored pointer
+	//! @return stored pointer
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	value_type* get() const;
+
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Swap
+	//! @param other unique_ptr
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	void swap(unique_ptr& other);
+
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Releases the ownership of the managed object
+	//! @return stored pointer
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	value_type* release();
+
+private:
+	value_type* ptr_;
+};
+
+template<typename value_type>
+unique_ptr<value_type>::unique_ptr()
+	: ptr_(nullptr), count_(nullptr) {}
+
+template<typename value_type>
+unique_ptr<value_type>::unique_ptr(value_type* pointer)
+	: ptr_(pointer) {}
+
+template<typename value_type>
+unique_ptr<value_type>::~unique_ptr()
+{
+	delete ptr_;
+}
+
+template<typename value_type>
+typename value_type unique_ptr<value_type>::operator*() const
+{
+	return *ptr_;
+}
+
+template<typename value_type>
+typename value_type* unique_ptr<value_type>::get() const
+{
+	return ptr_;
+}
+
+template<typename value_type>
+void unique_ptr<value_type>::swap(unique_ptr& other)
+{
+	std::swap(ptr_, other.ptr_);
+}
+
+template<typename value_type>
+typename value_type* unique_ptr<value_type>::release()
+{
+	value_type* pointer = ptr_;
+	ptr_ = nullptr;
+	return pointer;
+}
