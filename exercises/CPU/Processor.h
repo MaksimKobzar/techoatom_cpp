ac10737cae1bcf7bf8946e26e6fb626c3b345f4d
MaximKobzar <wtf__wow@mail.ru>
2017-04-02 17:20:32 +0300

    implement run, constr, descruct

--- exercises/CPU/Processor.h
+++ exercises/CPU/Processor.h
@@ -10,6 +10,7 @@
 #define _PROCESSOR_H_
 
 #include <iostream>
+#include "Switcher.h"
 
 //#define NDEBUG
 
@@ -21,27 +22,59 @@ namespace cpuns
 {
 
     template<
-    	class T,
-      class StackType = std::stack<T>
+    	typename T,
+    	class StackType = std::stack<T>
     >class Processor
 		{
-			using REG_NUM = 8;
+			using REG_NUM = 32;
+			using value_type = T;
+			using MEM_WIDTH = 256;
 		public:
 			explicit Processor();
 			Processor &Processor(const Processor &other);
 			~Processor();
-			bool fillMemory();
 
+			bool fillMemory();
+			bool run();
 
 
 		private:
-			StackType 				stack_;
-			Switcher 				switcher_;
-			T 						reg_[REG_NUM];
-			T 						temp_[REG_NUM];
-			std::deque<unsigned> 	cmdMemory_;
+			std::array<type_value, REG_NUM> 	*regs_;
+			std::deque<type_value> 				*cmdMemory_;
+			StackType 							*stack_;
+			Switcher<type_value> 				*switcher_;
 		};
 
 } // end cpuns
+
+template <typename value_type, class StackType>
+Processor<value_type, StackType>::Processor()
+{
+	regs_ 		= new std::array<type_value, REG_NUM>;
+	cmdMemory_ 	= new std::deque<type_value>(MEM_WIDTH);
+	stack_ 		= new StackType(10);
+	switcher_ 	= new StaSwitcher<type_value>(cmdMemory_, stack_, regs_);
+}
+
+template <typename value_type, class StackType>
+Processor<value_type, StackType>::~Processor()
+{
+	delete switcher_;
+	delete cmdMemory_;
+	delete stack_;
+	delete regs_;
+}
+
+template <typename value_type, class StackType>
+bool Processor<value_type, StackType>::fillMemory()
+{
+	switcher_->run();
+}
+
+template <typename value_type, class StackType>
+bool Processor<value_type, StackType>::run()
+{
+	switcher_->run();
+}
 
 #endif //_PROCESSOR_H_
--- exercises/CPU/Switcher.h
+++ exercises/CPU/Switcher.h
@@ -13,34 +13,102 @@
 #include <sstream>
 #include <string>
 
-    struct Switcher
+    template<
+        typename T = unsigned
+    >class Switcher
     {
-        using LABEL_TABLE_WIDTH = 10;
-        using HALF_WORD = 16;
+        using LABEL_TABLE_WIDTH = 32;
+        using HALF_CMD = 16;
+        using REG_NUM_WIDTH = 5;
+        using INSTR_WIDTH = 6;
+        using value_type =  T;
         //---------------------------------------------
         //! @Constructor
         //! Create class Switcher
         //---------------------------------------------
-        explicit Switcher();
-        explicit Switcher(const std::deque<unsigned> *other);
+        Switcher(const std::deque<value_type> *otherMemory, std::array<value_type> *otherRegs);
         Switcher(Switcher &other) = delete;
+        ~Switcher();
+
+        //---------------------------------------------
+        //! @Constructor
+        //! Get different fields of comand word
+        //---------------------------------------------
+        type_value getFieldInstr(value_type word);
+        type_value getFieldR0(value_type word);
+        type_value getFieldR1(value_type word);
+        type_value getFieldData(value_type word);
 
         //---------------------------------------------
         //! @Operators:
         //! operator =
         //! operator<<
         //---------------------------------------------
-        bool setMemory(const std::deque<unsigned> *other);
+        bool setMemory(const std::deque<value_type> *other);
         bool run();
 
     private:
-        const std::deque<unsigned>           *cmdMemoryPtr_;
+        const std::deque<value_type>         *cmdMemoryPtr_;
+        std::array<value_type>               *regs_;
         size_t                                cmdCounter_;
         std::array<size_t, LABEL_TABLE_WIDTH> labelTable_;
     };
 #endif // _SWITCHER_H_
 
-    bool Switcher::setMemory(const std::deque<unsigned> *memPtr)
+    template <typename value_type>
+    Switcher::Switcher(const std::deque<value_type> *otherMemory, std::array<value_type> *otherRegs)
+        : cmdMemoryPtr_ (otherMemory), regs_ (otherRegs), cmdCounter_(0)
+    {
+        #ifdef NDEBUG
+            clearLableTable();
+        #endif
+    }
+
+    template <typename value_type>
+    Switcher<value_type>::~Switcher()
+    {
+        #ifdef NDEBUG
+            clearLableTable();
+            cmdCounter_ = 0;
+        #endif
+    }
+
+    template <typename value_type>
+    value_type Switcher<value_type>::getFieldInstr(value_type word)
+    {
+        return(word >> (HALF_CMD + 2*REG_NUM_WIDTH));
+    }
+
+    template <typename value_type>
+    value_type Switcher::getFieldR0(value_type word)
+    {
+        return((word << INSTR_WIDTH) >> (HALF_CMD + REG_NUM_WIDTH + INSTR_WIDTH));
+    }
+
+    template <typename value_type>
+    value_type Switcher::getFieldR1(value_type word)
+    {
+        return((word << REG_NUM_WIDTH + INSTR_WIDTH) >> (HALF_CMD + REG_NUM_WIDTH + INSTR_WIDTH));
+    }
+
+    template <typename value_type>
+    value_type Switcher::getFieldData(value_type word)
+    {
+        value_type instr = getFieldInstr(word);
+        if( instr == J    ||
+            instr == JAL  ||
+        )
+        {
+            return(word & ((1 << (2*REG_NUM_WIDTH + HALF_CMD)) - 1));
+        }
+        else
+        {
+            return(word & ((1 << HALF_CMD) - 1));
+        }
+    }
+
+    template <typename value_type>
+    bool Switcher::setMemory(const std::deque<value_type> *memPtr)
     {
         if(memPtr == nullptr)
         {
@@ -53,6 +121,7 @@
         }
     }
 
+    template <typename value_type>
     bool Switcher::run()
     {
         if(cmdCounter_ != 0)
@@ -60,11 +129,14 @@
             return false;
         }
         fillLabelTable();
-        processCmds();
+        if(processCmds())
+        {
+            return false;
+        }
         return true;
     }
 
-
+    template <typename value_type>
     void clearLableTable()
     {
         for(auto i : labelTable_)
@@ -73,37 +145,49 @@
         }
     }
 
+    template <typename value_type>
     void Switcher::fillLabelTable()
     {
         clearLableTable();
-        while(*(cmdMemoryPtr_ + cmdCounter_) != '\0')
+        while(cmdMemoryPtr_[cmdCounter_]) != '\0')
         {
-            if(*(cmdMemoryPtr_ + cmdCounter_) & ((1 << HALF_WORD) - 1) == ':')
+            if(cmdMemoryPtr_[cmdCounter_] & ((1 << HALF_CMD) - 1) == ':')
             {
-                labelTable_[*(cmdMemoryPtr_ + cmdCounter_) >> HALF_WORD] = cmdCounter_;
+                labelTable_[cmdMemoryPtr_[cmdCounter_] >> HALF_CMD] = cmdCounter_;
             }
             ++cmdCounter_;
         }
         cmdCounter_ = 0;
     }
 
-
+    template <typename value_type>
     char Switcher::processCmds()
     {
-        unsigned cmdWord = 0;
+        T cmdWord = 0;
         while((cmdWord = *(cmdMemoryPtr_ + cmdCounter_)) != '\0')
         {
-            switch(cmdWord >> HALF_WORD)
+            switch(cmdWord >> HALF_CMD)
             {
+                case PUSH_CMD :
+                {
+                    stack_->push_back(getFieldData(cmdWord));
+                }
+                break;
+                case POP_CMD :
+                {
+                    reg_[getFieldR0(cmdWord)] = stack_->front();
+                    stack_->pop_front();
+                }
+                break;
                 case ADD_CMD :
                 {
                     if(stack_->size() < 2)
                     {
                         return 1; // return Error Code
                     }
-                    value_type op1 = stack_->front();
+                    T op1 = stack_->front();
                     stack_->pop_front();
-                    value_type op2 = stack_->front();
+                    T op2 = stack_->front();
                     stack_->pop_front();
                     stack_->push_back(op1 + op2);
                 }
@@ -114,9 +198,9 @@
                     {
                         return 1; // return Error Code
                     }
-                    value_type op1 = stack_->front();
+                    T op1 = stack_->front();
                     stack_->pop_front();
-                    stack_->push_back(op1 + (cmdWord & ((1 << HALF_WORD) - 1)));
+                    stack_->push_back(op1 + (cmdWord & ((1 << HALF_CMD) - 1)));
                 }
                 break;
                 case AND_CMD :
@@ -125,9 +209,9 @@
                     {
                         return 1; // return Error Code
                     }
-                    value_type op1 = stack_->front();
+                    T op1 = stack_->front();
                     stack_->pop_front();
-                    value_type op2 = stack_->front();
+                    T op2 = stack_->front();
                     stack_->pop_front();
                     stack_->push_back(op1 & op2);
                 }
@@ -148,8 +232,72 @@
                     }
                 }
                 break;
-                
+                case BGTZ_CMD :
+                {
+                    if(getFieldR0(cmdWord) > 0)
+                    {
+                        cmdCounter_ += getFieldData();
+                    }
+                }
+                break;
+                case J_CMD :
+                {
+                    if(0 <= getFieldData(cmdWord) && getFieldData(cmdWord) < 32)
+                    {
+                        cmdCounter_ = labelTable_[getFieldData()];
+                    }
+                    else
+                    {
+                        return 2; // return Error Code
+                    }
+                }
+                break;
+                case JAL_CMD :
+                {
+                    cmdCounter_ = getFieldData();
+                }
+                break;
+                case JR_CMD :
+                {
+                    cmdCounter_ = reg_[getFieldR0(cmdWord)];
+                }
+                break;
+                case MULT_CMD :
+                {
+                    if(stack_->size() < 2)
+                    {
+                        return 1; // return Error Code
+                    }
+                    T op1 = stack_->front();
+                    stack_->pop_front();
+                    T op2 = stack_->front();
+                    stack_->pop_front();
+                    stack_->push_back(op1 * op2);
+                }
+                break;
+                case NOOP_CMD :
+                {
+                }
+                break;
+
+        /*
+        *    case J_CMD :
+        *    {
+        *        if(0 <= getFieldData(cmdWord) && getFieldData(cmdWord) < 32)
+        *        {
+        *            cmdCounter_ = labelTable_[getFieldData()];
+        *        }
+        *        else
+        *        {
+        *            return 2; // return Error Code
+        *        }
+        *    }
+        *    break;
+        */
+
+
             }
+            ++cmdCounter_;
         }
         cmdCounter_ = 0;
         return 0;
