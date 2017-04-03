ccf00a52b0ca2000310f1de54ac63eb8b507fd80
MaximKobzar <wtf__wow@mail.ru>
2017-04-03 23:42:12 +0300

    add cmd_defines.h

---
+++ exercises/CPU/cmd_defines.h
@@ -0,0 +1,21 @@
+`define NOOP_CMD
+// arithmetic
+`define ADD_CMD  	0x0
+`define ADDI_CMD 	0x1
+`define MULT_CMD  	0x2	 
+`define DIV_CMD  	0x2	 
+`define sub_CMD  	0x2	 
+// logic
+`define AND_CMD  	0x2	 
+`define ANDI_CMD 	0x3
+`define OR_CMD  	0x2	 
+`define ORI_CMD 	0x3
+// jumps
+`define J_CMD
+`define JAL_CMD
+`define JR_CMD
+// compares
+`define BEQ_CMD  	0x4	
+`define BGEZ_CMD 	0x5
+`define BGTZ_CMD 	0x6
+`define MULT_CMD
